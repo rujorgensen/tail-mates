c4c925ae032e625cc1610e1b3537c2d4063ce029
rj <rj@oxyguard.dk>
2025-06-12 14:06:51 +0200

    feat: add better-auth utility functions to front- and backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,40 @@
-import { Elysia } from "elysia";
+import { type Context, Elysia } from 'elysia';
+import { auth } from './auth';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// User middleware (compute user and session and pass to routes)
+const betterAuth = new Elysia({ name: 'better-auth' })
+  .all('/api/auth/*', (context: Context) => {
+    if (['POST', 'GET'].includes(context.request.method)) {
+      return auth.handler(context.request);
+    }
+
+    context.status(405);
+  })
+
+  .macro({
+    auth: {
+      async resolve({ status, request: { headers } }) {
+        const session = await auth.api.getSession({
+          headers,
+        });
+
+        if (!session) {
+          return status(401);
+        }
+
+        return {
+          user: session.user,
+          session: session.session,
+        };
+      },
+    },
+  });
+
+const app = new Elysia()
+  .use(betterAuth)
+  .get("/", () => "Hello Elysia")
+  .listen(3000)
+  ;
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
