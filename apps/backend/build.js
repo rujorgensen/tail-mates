1184d05e6d1565e065501b613744bd27f7962388
rj <rj@oxyguard.dk>
2025-06-15 18:50:48 +0200

    feat: add build capabilities for Elysia backend

---
+++ apps/backend/build.js
@@ -0,0 +1,22 @@
+import { parseArgs } from 'bun:util';
+
+// Pass the project root as an argument to the build script
+const { values } = parseArgs({
+    args: Bun.argv,
+    options: {
+        projectRoot: {
+            type: 'string',
+        }
+    },
+    strict: true,
+    allowPositionals: true,
+});
+
+// Build the project using Bun
+await Bun.build({
+    entrypoints: [`./${values.projectRoot}/src/main.ts`],
+    outdir: `./dist/${values.projectRoot}`,
+    target: 'bun', // Use 'bun' as the target for Bun's native build
+    minify: true,
+    // sourcemap: 'linked', // default 'none'
+});
