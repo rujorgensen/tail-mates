9f5a62f384f00eb25c8af4442112623c2b066c03
rj <rj@oxyguard.dk>
2025-06-26 08:57:39 +0200

    feat: check for environment variables on startup

--- libs/backend/auth/src/lib/auth.ts
+++ libs/backend/auth/src/lib/auth.ts
@@ -3,6 +3,27 @@ import { PrismaClient } from '@prisma-types/tail-mates';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { userRegistered } from './discord.js';
 
+if (!process.env['GOOGLE_CLIENT_ID']) {
+	throw new Error('GOOGLE_CLIENT_ID is not set in environment variables');
+}
+const googleClientId: string = process.env['GOOGLE_CLIENT_ID'];
+
+if (!process.env['GOOGLE_CLIENT_SECRET']) {
+	throw new Error('GOOGLE_CLIENT_SECRET is not set in environment variables');
+}
+const googleClientSecret: string = process.env['GOOGLE_CLIENT_SECRET'];
+
+if (!process.env['FACEBOOK_CLIENT_ID']) {
+	throw new Error('FACEBOOK_CLIENT_ID is not set in environment variables');
+}
+const facebookClientId: string = process.env['FACEBOOK_CLIENT_ID'];
+
+if (!process.env['FACEBOOK_CLIENT_SECRET']) {
+	throw new Error('FACEBOOK_CLIENT_SECRET is not set in environment variables');
+}
+const facebookClientSecret: string = process.env['FACEBOOK_CLIENT_SECRET'];
+
+
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
@@ -17,18 +38,18 @@ export const auth = betterAuth({
 			create: {
 				after: async (user) => {
 					userRegistered(user);
-				}
+				},
 			},
 		},
 	},
 	socialProviders: {
 		google: {
-			clientId: process.env['GOOGLE_CLIENT_ID'] as string,
-			clientSecret: process.env['GOOGLE_CLIENT_SECRET'] as string,
+			clientId: googleClientId,
+			clientSecret: googleClientSecret,
 		},
 		facebook: {
-			clientId: process.env['FACEBOOK_CLIENT_ID'] as string,
-			clientSecret: process.env['FACEBOOK_CLIENT_SECRET'] as string,
+			clientId: facebookClientId,
+			clientSecret: facebookClientSecret,
 		},
 	},
 });
