7ddc7d95849b1232bc478786192271bc6890bafb
rj <rj@oxyguard.dk>
2025-06-26 09:44:42 +0200

    chore: format

--- apps/frontend/src/pages/ping.ts
+++ apps/frontend/src/pages/ping.ts
@@ -1,14 +1,17 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async () => {
-    return new Response(JSON.stringify({
-        status: 'ok',
-        message: 'pong',
-        timestamp: new Date().toISOString(),
-    }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+	return new Response(
+		JSON.stringify({
+			status: 'ok',
+			message: 'pong',
+			timestamp: new Date().toISOString(),
+		}),
+		{
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		},
+	);
 };
--- libs/backend/auth/src/lib/discord.ts
+++ libs/backend/auth/src/lib/discord.ts
@@ -1,14 +1,11 @@
-import {
-    WebhookClient,
-} from 'discord.js';
+import { WebhookClient } from 'discord.js';
 
 const webhookClient = new WebhookClient({
-    url: process.env['DISCORD_TAIL_MATES_WEBHOOK_URL'] ?? '-',
+	url: process.env['DISCORD_TAIL_MATES_WEBHOOK_URL'] ?? '-',
 });
 
 export const userRegistered = async (user: any) => {
-
-    webhookClient.send({
-        content: `New user registered 🥳!\n${JSON.stringify(user.username)}`,
-    });
-}
+	webhookClient.send({
+		content: `New user registered 🥳!\n${JSON.stringify(user.username)}`,
+	});
+};
