b38f18482667300f8c06848a9c279d0aaaae45fc
rj <rujorgensen@gmail.com>
2025-08-21 12:01:06 +0200

    fix: only set better auth baseURL in dev mode

--- apps/frontend/src/app/auth/auth-client.ts
+++ apps/frontend/src/app/auth/auth-client.ts
@@ -1,11 +1,10 @@
+import { isDevMode } from '@angular/core';
 import { createAuthClient } from 'better-auth/svelte';
 
 export const authClient = createAuthClient({
 	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: 'http://localhost:3100',
-	// baseURL: import.meta.env.PROD ? undefined : 'http://localhost:3100',
+	baseURL: isDevMode() ? 'http://localhost:3100' : undefined,
 });
-
 
 // Create demo user
 /*
